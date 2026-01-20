42e2cf96487968e396e5ad1df05cbb396a05faf4
Pawel Akonom <pawel.akonom@gmail.com>
2026-01-20 19:22:30 +0100

    Updated samba-server.sh script so it shows information how to connec Added imagemagick convert tool with all required libraries Added fbi tool with all required libraries Added htop tool with all required libraries

--- Apps/Samba/samba-server.sh
+++ Apps/Samba/samba-server.sh
@@ -5,10 +5,14 @@ SAMBA_LIB_DIR="/var/lib/samba/private"
 
 SMB_USER="root"
 SMB_PASS="trimui"
+SMB_HOST=$(grep -E '^[[:blank:]]*netbios name' /mnt/SDCARD/System/etc/samba/smb.conf | awk '{print $4}')
+HOST_IP=$(ifconfig wlan0 | grep -E '^[[:blank:]]*inet ' | awk '{print $2}' | cut -d: -f2)
 
-MSG_BACKGROUND="/mnt/SDCARD/System/res/background.png"
+BGR_FILE="/mnt/SDCARD/System/res/background.png"
+SMB_FILE="/mnt/SDCARD/System/res/samba-info.png"
 MSG_FONT="/mnt/SDCARD/System/res/DejaVuSansCondensed.ttf"
-MSG_COLOR="255,218,3"
+MSG_COLOR_HEX="#FFDA03"
+INFO_COLOR_HEX="#FFFFFF"
 
 export PATH=/mnt/SDCARD/System/bin:$PATH
 export LD_LIBRARY_PATH=/mnt/SDCARD/System/lib
@@ -30,13 +34,21 @@ echo -e "$SMB_PASS\n$SMB_PASS" | smbpasswd -a -s "$SMB_USER"
 if [ ! -f "/var/run/samba/smbd.pid" ]; then
   smbd -D
   nmbd -D
-  timeout 3 sdl2imgshow -i $MSG_BACKGROUND -f $MSG_FONT -s 50 -c "$MSG_COLOR" -t "Samba server started"
+  MSG_TEXT="Samba server started"
 else
   kill $(cat /var/run/samba/smbd.pid)
   kill $(cat /var/run/samba/nmbd.pid)
   smbd -D
   nmbd -D
-  timeout 3 sdl2imgshow -i $MSG_BACKGROUND -f $MSG_FONT -s 50 -c "$MSG_COLOR" -t "Samba server restarted" 
+  MSG_TEXT="Samba server restarted"
 fi
 
+SMB_INFO="NetBios name:  $SMB_HOST                           
+IP address:        $HOST_IP                                      
+Login:                $SMB_USER                                     
+Password:          $SMB_PASS"                                    
+                                                            
+convert "$BGR_FILE" -font "$MSG_FONT" -pointsize 40 -fill "$INFO_COLOR_HEX" -gravity SouthWest -annotate +360+80 "$SMB_INFO" "$SMB_FILE"
+convert "$SMB_FILE" -font "$MSG_FONT" -pointsize 60 -fill "$MSG_COLOR_HEX" -gravity North -annotate +0+140 "$MSG_TEXT" "$SMB_FILE"
+timeout 5 sdl2imgshow -i "$SMB_FILE" -f $MSG_FONT -s 10 -c '0,0,0' -t ' '
 
