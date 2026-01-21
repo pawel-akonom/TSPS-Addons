8274eaf80554a5ba2974f8a26fd04a541feba922
Pawel Akonom <pawel.akonom@gmail.com>
2026-01-21 15:28:16 +0100

    Added all available Libretro SNES Emualatores

--- Emus/SFC/snes9x2002.sh
+++ Emus/SFC/snes9x2002.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
-cpufreq.sh ondemand 4 7
-
+echo "===================================="
+echo $0 $*
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/SFC
 cd $RA_DIR/
 
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/snes9x2002_libretro.so "$@"
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/snes9x2002_libretro.so "$*"
--- Emus/SFC/snes9x2005_plus.sh
+++ Emus/SFC/snes9x2005_plus.sh
@@ -5,4 +5,4 @@ RA_DIR=/mnt/SDCARD/RetroArch
 EMU_DIR=/mnt/SDCARD/Emus/SFC
 cd $RA_DIR/
 
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/snes9x2005_libretro.so "$*"
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/snes9x2005_plus_libretro.so "$*"
