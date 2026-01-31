6f6312b1c502644f5e2076678dc5bf55082a3dfc
Pawel Akonom <pawel.akonom@gmail.com>
2026-01-31 11:43:20 +0100

    Update Screech launch.sh
    Fixed problem with missing root certificate

--- Apps/Screech/launch.sh
+++ Apps/Screech/launch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-progdir=$(dirname "$0")
-cd $progdir
+APPDIR=$(dirname "$0")
+CACERT="/etc/ssl/certs/ca-bundle.crt"
+if ! [ -f "$CACERT" ]; then
+  curl -Lk -o "$CACERT" https://curl.se/ca/cacert.pem
+fi
+cd $APPDIR
 ./screech &> debug.txt
